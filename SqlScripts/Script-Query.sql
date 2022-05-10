0ffbfe2f128f4ddff89b6ffa1ed3cc00846bc0c0
hoan.le <hoan.le@saigontechnology.com>
2022-05-10 08:48:25 +0700

    sync

--- SqlScripts/Script-Query.sql
+++ SqlScripts/Script-Query.sql
@@ -1,3 +1,22 @@
 select count(1) from customer
 select count(1) from customerscore
 select * from customer c 
+select * from customerscore 
+select * from admincampaign 
+
+-- delete from customer where CustomerID = 7
+-- delete from customerscore where CustomerScoreID = 7
+
+insert into AdminCampaign (CampaignName,CampaignDate,Status,LastUpdatedBy,LastUpdatedON) values ('Just Do It', '2021/8/26', 1, null, null);
+insert into AdminCampaign (CampaignName,CampaignDate,Status,LastUpdatedBy,LastUpdatedON) values ('Get a Mac', '2021/4/1', 1, null, null);
+insert into AdminCampaign (CampaignName,CampaignDate,Status,LastUpdatedBy,LastUpdatedON) values ('The Bottle', '2021/9/14', 1, null, null);
+insert into AdminCampaign (CampaignName,CampaignDate,Status,LastUpdatedBy,LastUpdatedON) values ('Stratos', '2022/2/6', 1, null, null);
+insert into AdminCampaign (CampaignName,CampaignDate,Status,LastUpdatedBy,LastUpdatedON) values ('The Marlboro Man', '2021/10/11', 1, null, null);
+insert into AdminCampaign (CampaignName,CampaignDate,Status,LastUpdatedBy,LastUpdatedON) values ('The Most Interesting Man in the World', '2021/6/25', 1, null, null);
+insert into AdminCampaign (CampaignName,CampaignDate,Status,LastUpdatedBy,LastUpdatedON) values ('Flo', '2021/12/25', 1, null, null);
+insert into AdminCampaign (CampaignName,CampaignDate,Status,LastUpdatedBy,LastUpdatedON) values ('Got Milk?', '2022/3/16', 1, null, null);
+insert into AdminCampaign (CampaignName,CampaignDate,Status,LastUpdatedBy,LastUpdatedON) values ('Real Beauty', '2022/1/30', 1, null, null);
+insert into AdminCampaign (CampaignName,CampaignDate,Status,LastUpdatedBy,LastUpdatedON) values ('Back to the Start', '2021/4/30', 1, null, null);
+insert into AdminCampaign (CampaignName,CampaignDate,Status,LastUpdatedBy,LastUpdatedON) values ('Wassup?', '2021/8/6', 1, null, null);
+insert into AdminCampaign (CampaignName,CampaignDate,Status,LastUpdatedBy,LastUpdatedON) values ('SOS - Start on saving', '2022/2/28', 1, null, null);
+insert into AdminCampaign (CampaignName,CampaignDate,Status,LastUpdatedBy,LastUpdatedON) values ('WWI - Why Waste It', '2021/11/20', 1, null, null);
